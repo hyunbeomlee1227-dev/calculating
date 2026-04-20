1a118bf337575c8cd30c29f9568f4f5ea254b5ce
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-04-20 19:27:49 +0900

    [feat] minus 확인 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,13 +2,53 @@ package com.ll;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calc {
+    static List<String> tokens = new ArrayList<>();
+    static int pos = 0;
+
     public static int run(String expression) {
-        return 0;
+        tokens.clear();
+        pos = 0;
+
+        Pattern pattern = Pattern.compile("\\d+|[+\\-*/()]");
+        Matcher matcher = pattern.matcher(expression);
+
+        while (matcher.find()) {
+            tokens.add(matcher.group());
+        }
+
+        ChangeMinus();
+
+        return Calc();
+    }
+
+    public static int Calc() {
+
+    }
+
+    public static void ChangeMinus() {
+        for (int i = 0; i < tokens.size(); ++i) {
+            if (!tokens.get(i).equals("-")) continue;
+
+            if (i == 0 || isOperator(tokens.get(i - 1))) {
+                if (i + 1 < tokens.size()) {
+                    tokens.set(i + 1, "-" + tokens.get(i + 1));
+                    tokens.remove(i);
+                    i--;
+                }
+            }
+        }
+    }
+
+    static boolean isOperator(String minusCheck) {
+        return minusCheck.equals("+")
+                || minusCheck.equals("-")
+                || minusCheck.equals("*")
+                || minusCheck.equals("/")
+                || minusCheck.equals("(");
     }
 }
 
