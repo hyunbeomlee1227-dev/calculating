7e32eac7bd5167f76268d6a3adb368d949abd101
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-04-20 19:00:06 +0900

    [reset] reset history

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "com.ll"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.assertj:assertj-core:3.27.7")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,14 @@
+package com.ll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calc {
+    public static int run(String expression) {
+        return 0;
+    }
+}
+
